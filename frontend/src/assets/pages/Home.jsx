7e68f236a052b3609e9b3de01ef3f58cea5c78ca
Sudhanshu kumar <sk3360240@gmail.com>
2026-08-02 02:18:43 +0530

    chore: update footer errors for production

--- frontend/src/assets/pages/Home.jsx
+++ frontend/src/assets/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Services from "../components/Services";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 import About from "../components/About";
 
 
