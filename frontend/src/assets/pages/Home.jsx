4a78c47f0ca9221368696e7babf0222933231a7f
Sudhanshu kumar <sk3360240@gmail.com>
2026-08-02 02:15:52 +0530

    chore: update again frontend errors for production

--- frontend/src/assets/pages/Home.jsx
+++ frontend/src/assets/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Services from "../components/Services";
-import Footer from "../components/Footer";
+import Footer from "../components/footer";
 import About from "../components/About";
 
 
